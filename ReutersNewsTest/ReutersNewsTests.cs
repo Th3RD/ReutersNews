1b5b2aa6cb4e884d1868e1d8b14b4902d855ce7e
Th3RD <gleb_gavrilovich@epam.com>
2016-05-30 14:29:30 +0300

    v1

---
+++ ReutersNewsTest/ReutersNewsTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Mail;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace ReutersNewsTest
+{
+	[TestClass]
+	public class ReutersNewsTests
+	{
+		#region Fields & Properties
+		public static string Result { get; set; }
+		public static string Subject { get; set; }
+		public static bool CommonPassed { get; set; }
+		public TestContext TestContext { get; set; }
+		private static IWebDriver _driver;
+		#endregion
+		
+		#region Initializing
+		[ClassInitialize]
+		public static void Init(TestContext testContext)
+		{
+			_driver = new FirefoxDriver();
+			CommonPassed = true;
+			Result = string.Empty;
+			Subject = "Test Results of MagicNews/NewsMax stability";
+		}
+		#endregion
+
+		#region Tests
+		[TestMethod]
+		public void MagicNews()
+		{
+			_driver.Navigate().GoToUrl("http://217.21.63.69/#/magic");
+			WaitForElement(_driver, By.CssSelector(".accordionitem.ng-scope"));
+			_driver.FindElement(By.CssSelector(".accordionitem.ng-scope")).Click();
+			_driver.FindElement(By.Id("uploadBtn")).SendKeys(Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory())) + "\\test-file.xml");
+			_driver.FindElement(By.CssSelector(".flex-item.submitbutton")).Click();
+
+			WaitForElement(_driver, By.XPath("//*[contains(@class, 'result-item repeated-item')]"));
+			var result = _driver.FindElement(By.XPath("//input[@name='range0']"));
+			var value = result.GetAttribute("value");
+
+			Assert.IsTrue(value.Equals("182 results"));
+		}
+
+		[TestMethod]
+		public void NewsMax()
+		{
+			_driver.Navigate().GoToUrl("http://217.21.63.69/#/newsmax");
+			WaitForElement(_driver, By.CssSelector(".accordionitem.V2.ng-scope"));
+			_driver.FindElement(By.CssSelector(".accordionitem.V2.ng-scope")).Click();
+			_driver.FindElement(By.Id("uploadBtn")).SendKeys(Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory())) + "\\test-file.xml");
+			_driver.FindElement(By.CssSelector(".flex-item.submitbutton")).Click();
+
+			WaitForElement(_driver, By.XPath("//*[contains(@class, 'result-item repeated-item')]"));
+			var result = _driver.FindElement(By.XPath("//input[@name='range0']"));
+			var value = result.GetAttribute("value");
+
+			Assert.IsTrue(value.Equals("182 results"));
+		}
+		#endregion
+
+		#region Cleanup
+		[TestCleanup]
+		public void CollectReport()
+		{
+			if (TestContext.CurrentTestOutcome == UnitTestOutcome.Failed)
+			{
+				CommonPassed = false;
+				Result += string.Format("{0} test was failed. Possibly there is a problem. Please check it ASAP!!!\n", TestContext.TestName);
+			}
+			else
+			{
+				Result += string.Format("{0} test passed. Looks like everything works fine.\n", TestContext.TestName);
+			}
+		}
+		
+		[ClassCleanup]
+		public static void SendReport()
+		{
+			_driver.Quit();
+			Subject = (CommonPassed ? "PASSED: " : "FAILED: ") + Subject;
+			SendMail();
+		}
+		#endregion
+
+		#region Methods
+		public static void SendMail()
+		{
+			SmtpClient client = new SmtpClient
+			{
+				Port = 587,
+				Host = "smtp.gmail.com",
+				EnableSsl = true,
+				Timeout = 10000,
+				DeliveryMethod = SmtpDeliveryMethod.Network,
+				UseDefaultCredentials = false,
+				Credentials = new System.Net.NetworkCredential("magicnewstest@gmail.com", "magicPassword")
+			};
+
+			MailMessage mm = new MailMessage("hleb.haurylovich@gmail.com", "gleb_gavrilovich@epam.com", Subject, Result)
+			{
+				BodyEncoding = Encoding.UTF8,
+				DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure
+			};
+
+			client.Send(mm);
+		}
+
+		public static void WaitForElement(IWebDriver driver, By by, int seconds = 30)
+		{
+			var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+			wait.Until(d => d.FindElement(by).Displayed);
+		}
+		#endregion
+	}
+}
